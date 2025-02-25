1e92f3daf5a5c2089af5b28ad7d97385493fde4c
MonkeyBalls232 <129045291+MonkeyBalls232@users.noreply.github.com>
2025-02-25 18:30:47 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2757,6 +2757,78 @@ do
     Library.WatermarkText = WatermarkLabel;
     Library:MakeDraggable(Library.Watermark);
 
+    local LoaderOuter = Library:Create('Frame', {
+        BorderColor3 = Color3.new(0, 0, 0);
+        Position = UDim2.new(0.5, -106, 0.5, -10); -- Centered on screen
+        Size = UDim2.new(0, 213, 0, 20);
+        ZIndex = 200;
+        Parent = ScreenGui;
+    });
+
+    local LoaderInner = Library:Create('Frame', {
+        BackgroundColor3 = Library.MainColor;
+        BorderColor3 = Library.AccentColor;
+        BorderMode = Enum.BorderMode.Inset;
+        Size = UDim2.new(0, 0, 1, 0); -- Initially 0 width
+        ZIndex = 201;
+        Parent = LoaderOuter;
+    });
+    
+    Library:AddToRegistry(LoaderInner, {
+        BackgroundColor3 = 'MainColor';
+        BorderColor3 = 'AccentColor';
+    });
+
+    local InnerFrame = Library:Create('Frame', {
+        BackgroundColor3 = Color3.new(1, 1, 1);
+        BorderSizePixel = 0;
+        Position = UDim2.new(0, 1, 0, 1);
+        Size = UDim2.new(1, -2, 1, -2);
+        ZIndex = 202;
+        Parent = LoaderInner;
+    });
+    
+    local Gradient = Library:Create('UIGradient', {
+        Color = ColorSequence.new({
+            ColorSequenceKeypoint.new(0, Library:GetDarkerColor(Library.MainColor)),
+            ColorSequenceKeypoint.new(1, Library.MainColor),
+        });
+        Rotation = -90;
+        Parent = InnerFrame;
+    });
+
+    Library:AddToRegistry(Gradient, {
+        Color = function()
+            return ColorSequence.new({
+                ColorSequenceKeypoint.new(0, Library:GetDarkerColor(Library.MainColor)),
+                ColorSequenceKeypoint.new(1, Library.MainColor),
+            });
+        end
+    });
+    
+    -- Create a label to display the loading progress
+    local LoaderLabel = Library:CreateLabel({
+        Position = UDim2.new(0, 5, 0, 0);
+        Size = UDim2.new(1, -4, 1, 0);
+        TextSize = 14;
+        TextXAlignment = Enum.TextXAlignment.Left;
+        ZIndex = 203;
+        Parent = InnerFrame;
+    });
+    
+    Library.Loader = LoaderOuter;
+    Library.LoaderText = LoaderLabel;
+    
+    -- Example function to update the loading bar
+    function Library:UpdateLoader(progress)
+        LoaderInner.Size = UDim2.new(progress, 0, 1, 0); -- Update width based on progress
+        LoaderLabel.Text = string.format("Loading... %d%%", progress * 100);
+    end
+
+    -- Make the loader draggable
+    Library:MakeDraggable(Library.Loader);
+
+
 
 
     local KeybindOuter = Library:Create('Frame', {
